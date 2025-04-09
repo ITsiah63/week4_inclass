76948c2a49dfa71c4f01dbccfce13dd579a22cf9
Isiah Reyes <i0576631@amarillocollege.com>
2025-04-09 15:46:34 -0500

    second push

--- project05-01.js
+++ project05-01.js
@@ -4,8 +4,8 @@
       Project 05-01
 
       Project to present an online quiz with a countdown clock
-      Author: 
-      Date:   
+      Author: Isiah Reyes
+      Date:   04-09-25
 
       Filename: project05-01.js
 */
@@ -25,24 +25,39 @@ quizClock.value = quizTime;
 let timeLeft = quizTime;
 
 // Declare the ID for timed commands
+let timeID;
 // and the node list for questions
+let questionList = document.querySelectorAll("div#quiz input");
 
+startQuiz.onclick = function() {
+   overlay.className = "showquiz";
+   timeID = window.setInterval(countdown, 1000);
+}
 
+function countdown() {
+   if (timeLeft==0){
+      clearInterval(timeID)
+      let totalCorrect = checkAnswers()
 
+      if (totalCorrect==correctAnswers.length)
+      {
+         window.alert("Congratulations on getting 100%")
+             
+      }
+      else {
+         timeLeft = quizTime 
+         quizClock.value = timeLeft
+         overlay.className = "hidequiz"
+         window.alert("You answered correctly " + totalCorrect+ "out of "+correctAnswers.length+ "questions")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+      }
+   }
+   else (timeLeft != 0);{
+      timeLeft--;
+      quizClock.value = timeLeft
+   }
+   
+}
 
 
 
